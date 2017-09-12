3770a7ae58c97f42bafea397d8aa84b56fbbaef5
zhiyanwang <zywang999@gmail.com>
2017-09-11 20:17:17 -0400

    Adding python programs for Homework 1

---
+++ Homework 1 Question 1.py
@@ -0,0 +1,7 @@
+
+# Input height of the tower
+h = float(input("Please enter the height of the tower: "))
+
+# Calculate time for drop
+t = (2*h/9.81)**0.5
+print("the time for a ball dropped from a ", h, "m high tower is ", t, "seconds")
