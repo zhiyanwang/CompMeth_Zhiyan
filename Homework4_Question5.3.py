d9a4b892c819596159c83ee80a8c73f6db3348a8
zhiyanwang <zywang999@gmail.com>
2017-10-10 10:23:27 -0400

    Homework 4 - Integration

---
+++ Homework4_Question5.3.py
@@ -0,0 +1,34 @@
+import math
+import matplotlib.pyplot as plt
+
+def f(x):
+    return math.exp(-x**2)
+
+E_x = []
+x = []
+
+x_n = 0
+
+for x_n in range(0,500):
+
+    N = 30
+    a = 0.0
+    b = x_n/100
+    h = (b-a)/N
+    s = 0.5*f(a) + 0.5*f(b)
+
+    for k in range(1,N):
+        s += f(a+k*h)
+
+    E_x.append(h*s)
+    x.append(x_n/100)
+    x_n += 1
+
+print (len(x))
+print (len(E_x))
+
+plt.plot(x,E_x,"purple")
+plt.xlabel("x")
+plt.ylabel("E(x)")
+plt.title("E(x)")
+plt.show()
